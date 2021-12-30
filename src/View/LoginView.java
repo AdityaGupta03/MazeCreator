57e8c55c37babe69f2c6fa5e147e64a331ef1759
AdityaGupta03 <gupta742@purdue.edu>
2021-12-29 20:20:54 -0800

    Added LoginView GUI
    Signed-off-by: AdityaGupta03 <gupta742@purdue.edu>

--- src/View/LoginView.java
+++ src/View/LoginView.java
@@ -1,9 +1,137 @@
 package View;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Class which displays and handles the login/signup for the application
  */
-public class LoginView {
+public class LoginView extends JComponent implements Runnable {
+    JLabel welcomeText;
+    JLabel enterUsernameL;
+    JLabel enterPasswordL;
+    JLabel enterUsernameS;
+    JLabel enterPasswordS;
+    JLabel enterName;
+    JLabel or = new JLabel("Or");
+
+    JButton loginButton;
+    JButton signupButton;
+
+    JTextField lUsername = new JTextField(10);
+    JTextField sUsername = new JTextField(10);
+    JTextField lPassword = new JTextField(10);
+    JTextField sPassword = new JTextField(10);
+    JTextField name = new JTextField(10);
+
+    LoginView loginView;
+
+    ActionListener actionListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    };
+
+    public LoginView() {
+
+    }
+
+    public void run() {
+        JFrame frame = new JFrame();
+        frame.setTitle("MazeCreator");
+        Container content = frame.getContentPane();
+        content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
+
+        loginView = new LoginView();
+        content.add(loginView);
+        frame.setSize(700, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+
+        enterUsernameL = new JLabel("Enter Username:");
+        enterPasswordL = new JLabel("Enter Password:");
+        enterUsernameS = new JLabel("Enter Username:");
+        enterPasswordS = new JLabel("Enter Password:");
+        enterName = new JLabel("Enter Full Name:");
+        welcomeText = new JLabel("Welcome to MazeCreator!");
+        loginButton = new JButton("Log In");
+        signupButton = new JButton("Create Account");
+
+        JPanel welcomePanel = new JPanel();
+        welcomePanel.add(welcomeText);
+        content.add(welcomePanel, BorderLayout.PAGE_START);
+
+        JPanel optionPanel = new JPanel();
+
+        content.add(optionPanel, BorderLayout.CENTER);
+
+        JPanel loginPanel = new JPanel();
+
+        JPanel loginUsername = new JPanel();
+        loginUsername.add(enterUsernameL);
+        loginUsername.add(lUsername);
+        loginPanel.add(loginUsername);
+
+        JPanel loginPassword = new JPanel();
+        loginPassword.add(enterPasswordL);
+        loginPassword.add(lPassword);
+        loginPanel.add(loginPassword);
+
+        loginPanel.add(loginButton);
+
+        loginPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        loginPanel.setPreferredSize(new Dimension(300, 650));
+        loginPanel.setMaximumSize(new Dimension(500, 650));
+
+        optionPanel.add(loginPanel);
+
+        JPanel orPanel = new JPanel();
+        orPanel.add(or);
+
+        orPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        orPanel.setPreferredSize(new Dimension(50, 600));
+        orPanel.setMaximumSize(new Dimension(50, 600));
+
+        optionPanel.add(orPanel);
+
+        JPanel signupPanel = new JPanel();
+
+        JPanel signupName = new JPanel();
+        signupName.add(enterName);
+        signupName.add(name);
+        signupPanel.add(signupName);
+
+        JPanel signupUsername = new JPanel();
+        signupUsername.add(enterUsernameS);
+        signupUsername.add(sUsername);
+        signupPanel.add(signupUsername);
+
+        JPanel signupPassword = new JPanel();
+        signupPassword.add(enterPasswordS);
+        signupPassword.add(sPassword);
+        signupPanel.add(signupPassword);
+
+        signupPanel.add(signupButton);
+
+        signupPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        signupPanel.setPreferredSize(new Dimension(300, 700));
+        signupPanel.setMaximumSize(new Dimension(300, 700));
+
+        optionPanel.add(signupPanel);
+
+        optionPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        optionPanel.setPreferredSize(new Dimension(950, 500));
+        optionPanel.setMaximumSize(new Dimension(950, 500));
+
+        loginButton.addActionListener(actionListener);
+        signupButton.addActionListener(actionListener);
+    }
+
     public static void main(String[] args) {
-        System.out.println("test");
+        SwingUtilities.invokeLater(new LoginView());
     }
 }
