333d7518dfa11a2e722a9ecb300516cee3d8c66e
AdityaGupta03 <gupta742@purdue.edu>
2021-12-29 18:42:02 -0800

    Added basic application structure

---
+++ src/View/LoginView.java
@@ -0,0 +1,9 @@
+package View;
+/**
+ * Class which displays and handles the login/signup for the application
+ */
+public class LoginView {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
